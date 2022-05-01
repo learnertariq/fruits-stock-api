41f8ebbaa16ba4a788497ba3a8ca652a06b714bc
Tariqul Islam <usukuk@gmail.com>
2022-05-01 13:17:06 +0600

    patch in fruits

--- routes/fruits.js
+++ routes/fruits.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
+const mongoose = require("mongoose");
 const { Fruit } = require("../models/fruit");
 router.get("/", async (req, res) => {
   const fruits = await Fruit.find({});
   res.send(fruits);
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const fruit = await Fruit.findById(mongoose.Types.ObjectId(id));
+  res.send(fruit);
 });
 
 router.post("/", async (req, res) => {
@@ -18,6 +25,27 @@ router.post("/", async (req, res) => {
   });
 
   await fruit.save();
+  res.send(fruit);
+});
+
+router.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+  const bodyCopy = req.body;
+
+  const updatedFruit = {};
+  if (bodyCopy.name) updatedFruit.name = bodyCopy.name;
+  if (bodyCopy.quantity) updatedFruit.quantity = bodyCopy.quantity;
+  if (bodyCopy.supplier) updatedFruit.supplier = bodyCopy.supplier;
+  if (bodyCopy.price) updatedFruit.price = bodyCopy.price;
+  if (bodyCopy.desc) updatedFruit.desc = bodyCopy.desc;
+  if (bodyCopy.img) updatedFruit.img = bodyCopy.img;
+
+  const fruit = await Fruit.findByIdAndUpdate(
+    mongoose.Types.ObjectId(id),
+    updatedFruit,
+    { new: true }
+  );
+
   res.send(fruit);
 });
 
